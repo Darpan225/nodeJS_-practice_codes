c476fe58db87e0e389bba1c8135582c56f089dcd
Darpan <darpanbiswas225@gmail.com>
2025-10-20 21:54:27 +0530

    Program of datatype

---
+++ Basic_01/Q1.js
@@ -0,0 +1,25 @@
+let job = null
+console.log(job)
+console.log (typeof(job))
+
+let eID;
+console.log(eID)
+
+let Name = "Ram Das"
+console.log(Name)
+console.log(typeof(Name))
+
+let frnds = ['Raju', 'Laju', 'Komal' ]
+
+console.log (frnds)
+console.log(typeof(frnds))
+
+let natl = true
+console.log(natl)
+console.log(typeof(natl))
+
+let salary = 5115184658451511415111
+console.log(salary)
+console.log(typeof(salary))
+
+console.table([Name, salary, eID, job, natl])
