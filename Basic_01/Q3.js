0ea9d89b628e9cd42e160f4eb1c7c64270f442a1
Darpan <darpanbiswas225@gmail.com>
2025-10-21 13:13:39 +0530

    problem number 3

---
+++ Basic_01/Q3.js
@@ -0,0 +1,16 @@
+//Write a program to enter two numbers and perform all arithmetic operations.
+
+let a = 5
+let b = 2
+
+console.log ("Addition a+b = ", a+b)
+console.log("Subtraction a-b = ", a-b)
+console.log("Multiplication a*b = ", a*b)
+console.log("Division a/b = ", a/b)
+console.log("Modulus a%b = ",a%b)
+console.log("Exponentiation (power) a**2 = ", a**2 )
+console.log("Increment a ++ (postfix) = ", a++)
+console.log("Increment ++a (Prefix) = ", ++a)
+console.log("Decrement b-- (postfix) = ", b--)
+console.log("Decrement --b (prefix) = ", --b)
+
